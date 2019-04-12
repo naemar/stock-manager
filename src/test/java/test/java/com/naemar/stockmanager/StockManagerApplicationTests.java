4b4b00e7d7166664385ce69a655bed7cefbded36
naemar <guti2k@web.de>
2019-04-12 10:36:54 +0200

    fix test

--- src/test/java/test/java/com/naemar/stockmanager/StockManagerApplicationTests.java
+++ src/test/java/test/java/com/naemar/stockmanager/StockManagerApplicationTests.java
@@ -1,13 +1,21 @@
 package test.java.com.naemar.stockmanager;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import main.java.com.naemar.stockmanager.StockManagerApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StockManagerApplication.class)
 public class StockManagerApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue("This will succeed.", true);
 	}
 
 }
